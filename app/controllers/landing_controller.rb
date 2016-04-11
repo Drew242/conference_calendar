d229609d65fc99c3a1628f9af86a7204adea86ab
Drew Conly <atconly@gmail.com>
2016-04-10 22:24:12 -0600

    pseudo work

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,12 +1,22 @@
 class LandingController < ApplicationController
   def index
     @events = Event.all.where(day: first_day)
+    @next_day = @events.first.day
+  end
+
+  def next_day
+    @events = Event.all.where(day: last_day)
+    @next_day = @events.first.day
+    render :index
   end
 
   private
 
-    def first_day
-      Event.first.start_time.strftime("%m/%d")
-    end
+  def first_day
+    Event.first.start_time.strftime("%m/%d")
+  end
 
+  def last_day
+    Event.last.start_time.strftime("%m/%d")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root "landing#index"
+  # get  "landing#next_day"
+
+  get "/:next_day", to: "landing#next_day", as: "next_day"
 end
